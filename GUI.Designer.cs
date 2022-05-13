51db4fd861f80bebb3339db4451ac423cf3682bc
共和国工科生 <1335075427@qq.com>
2022-05-13 23:38:43 +0800

    增加中断按钮，增加自定义延时功能，增加模拟手工输入延迟，窗口大小可调

--- GUI.Designer.cs
+++ GUI.Designer.cs
@@ -32,14 +32,19 @@
             this.Input = new System.Windows.Forms.RichTextBox();
             this.Start = new System.Windows.Forms.Button();
             this.Help = new System.Windows.Forms.Button();
+            this.SimulateMode = new System.Windows.Forms.CheckBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.DelayTime = new System.Windows.Forms.TextBox();
+            this.Delay = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Input
             // 
-            this.Input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.Input.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.Input.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Input.Font = new System.Drawing.Font("Consolas", 12F);
             this.Input.Location = new System.Drawing.Point(12, 12);
             this.Input.Name = "Input";
             this.Input.Size = new System.Drawing.Size(760, 351);
@@ -49,10 +54,8 @@
             // 
             // Start
             // 
-            this.Start.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Start.Location = new System.Drawing.Point(350, 369);
+            this.Start.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Start.Location = new System.Drawing.Point(346, 369);
             this.Start.Name = "Start";
             this.Start.Size = new System.Drawing.Size(100, 30);
             this.Start.TabIndex = 1;
@@ -62,6 +65,7 @@
             // 
             // Help
             // 
+            this.Help.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.Help.Location = new System.Drawing.Point(672, 369);
             this.Help.Name = "Help";
             this.Help.Size = new System.Drawing.Size(100, 30);
@@ -70,19 +74,76 @@
             this.Help.UseVisualStyleBackColor = true;
             this.Help.Click += new System.EventHandler(this.Help_Click);
             // 
+            // SimulateMode
+            // 
+            this.SimulateMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.SimulateMode.AutoSize = true;
+            this.SimulateMode.Location = new System.Drawing.Point(12, 378);
+            this.SimulateMode.Name = "SimulateMode";
+            this.SimulateMode.Size = new System.Drawing.Size(123, 21);
+            this.SimulateMode.TabIndex = 3;
+            this.SimulateMode.Text = "模拟手工输入延迟";
+            this.SimulateMode.UseVisualStyleBackColor = true;
+            // 
+            // Stop
+            // 
+            this.Stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Stop.Location = new System.Drawing.Point(566, 369);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(100, 30);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "中断粘贴";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // DelayTime
+            // 
+            this.DelayTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DelayTime.Location = new System.Drawing.Point(206, 376);
+            this.DelayTime.Name = "DelayTime";
+            this.DelayTime.Size = new System.Drawing.Size(60, 23);
+            this.DelayTime.TabIndex = 5;
+            this.DelayTime.TextChanged += new System.EventHandler(this.DelayTime_TextChanged);
+            // 
+            // Delay
+            // 
+            this.Delay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Delay.AutoSize = true;
+            this.Delay.Location = new System.Drawing.Point(141, 379);
+            this.Delay.Name = "Delay";
+            this.Delay.Size = new System.Drawing.Size(68, 17);
+            this.Delay.TabIndex = 6;
+            this.Delay.Text = "粘贴延时：";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ms";
+            // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Delay);
+            this.Controls.Add(this.DelayTime);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.SimulateMode);
             this.Controls.Add(this.Help);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.Input);
-            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GUI";
             this.Text = "键盘模拟器";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,5 +152,10 @@
         private RichTextBox Input;
         private Button Start;
         private Button Help;
+        private CheckBox SimulateMode;
+        private Button Stop;
+        private TextBox DelayTime;
+        private Label Delay;
+        private Label label1;
     }
 }
--- GUI.cs
+++ GUI.cs
@@ -2,26 +2,40 @@ namespace KeyboardSimulator
 {
     public partial class GUI : Form
     {
+        private Thread Executer = null;
+        private int Time = 1000;
         public GUI()
         {
             InitializeComponent();
+            DelayTime.Text = "1000";
         }
-
         private void Input_TextChanged(object sender, EventArgs e)
         {
             Keyboard.Text = Input.Text;
         }
-
         private void Help_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("本软件用于模拟键盘输入，可模拟输入中文。\n" +
-                "你可以一次性键入所有要粘贴的内容到文本框内。\n" +
-                "点击 “粘贴！” 按钮后，将延迟三秒执行模拟输入。\n" +
-                "！！！！！请先切换成英文输入法！！！！！");
+            MessageBox.Show("！！！！！请先切换成英文输入法！！！！！\n" +
+                "\n\n" +
+                "本软件用于模拟键盘输入，可模拟输入中文。\n" +
+                "点击 「粘贴！」按钮后，将延迟一定时间后再执行模拟输入。\n" +
+                "\n" +
+                "另外，「中断粘贴」是在粘贴出现问题时强制终止的按钮。");
         }
-
         public static class Keyboard
         {
+            private static bool simulateMode = false;
+            private static int time = 1000;
+            public static int Time
+            {
+                get => time;
+                set => time = value;
+            }
+            public static bool SimulateMode
+            {
+                get => simulateMode;
+                set => simulateMode = value;
+            }
             private static string text = "";
             public static string Text
             {
@@ -31,7 +45,8 @@ namespace KeyboardSimulator
             public static void Simulator()
             {
                 string KeyStream = "";
-                for (int i = 0; i < Text.Length; i++)
+                string Temp = Text;
+                for (int i = 0; i < Temp.Length; i++)
                 {
                     switch (Text[i])
                     {
@@ -54,19 +69,62 @@ namespace KeyboardSimulator
                             KeyStream += "{)}";
                             break;
                         default:
-                            KeyStream += Text[i];
+                            KeyStream += Temp[i];
                             break;
                     }
                 }
-                Thread.Sleep(1000);
-                SendKeys.SendWait(KeyStream);
+                Thread.Sleep(Time);
+                if (!SimulateMode)
+                    SendKeys.SendWait(KeyStream);
+                else
+                {
+                    Random Generator = new Random();
+                    for (int i = 0; i < KeyStream.Length; i++)
+                    {
+                        for (int j = 0; j < Generator.Next(2, 6); j++)
+                        {
+                            if (i >= KeyStream.Length)
+                                break;
+                            SendKeys.SendWait("" + KeyStream[i]);
+                            i++;
+                        }
+                        Thread.Sleep(Generator.Next(500, 1000));
+                    }
+                }
             }
         }
-
         private void Start_Click(object sender, EventArgs e)
         {
-            Thread Executer = new(new ThreadStart(Keyboard.Simulator));
+            Keyboard.SimulateMode = SimulateMode.Checked;
+            Keyboard.Time = Time;
+            Executer = new Thread(new ThreadStart(Keyboard.Simulator));
             Executer.Start();
+            while (Executer.IsAlive)
+            {
+                Application.DoEvents();
+            }
+            MessageBox.Show("粘贴完成");
+        }
+        protected override void OnLayout(LayoutEventArgs levent)
+        {
+            base.OnLayout(levent);
+            Start.Size = new Size(100, 30);
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (Executer != null)
+                Executer.Abort();
+        }
+        private void DelayTime_TextChanged(object sender, EventArgs e)
+        {
+            if (DelayTime.Text.Length == 0) DelayTime.Text = "0";
+            if ('0' > DelayTime.Text[DelayTime.Text.Length - 1] || DelayTime.Text[DelayTime.Text.Length - 1] > '9')
+                DelayTime.Text = DelayTime.Text.Substring(0, DelayTime.Text.Length - 1);
+            else
+            {
+                if (!int.TryParse(DelayTime.Text, out Time))
+                    DelayTime.Text = "1000";
+            }
         }
     }
 }
